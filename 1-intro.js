a8823095ad8a95a838a21bedc89412a1f95eb079
liushangpeng <liushangpeng444@gmail.com>
2023-01-10 20:00:29 +0800

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const amount = 6
+
+if (amount < 10) {
+    console.log('small number');
+}
+else {
+    console.log('largee number');
+}
+
+console.log(`Hey this is my first node app.`);
---
+++ 5-utils.js
@@ -0,0 +1,7 @@
+const sayHi = (name) => {
+    console.log(`Hello beautiful ${name}`);
+}
+
+
+// export default
+module.exports = sayHi
